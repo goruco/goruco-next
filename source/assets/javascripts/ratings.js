2c75d3fd3b6eba5b98e7b1803637a7a52d89c5e0
Jordan Cooperman <jordancooperman@gmail.com>
2015-02-25 17:58:30 -0500

    complete overhaul of current styles

--- source/assets/javascripts/ratings.js
+++ source/assets/javascripts/ratings.js
@@ -2,8 +2,8 @@ var ratings = {};
 
 ratings.fetchRating = function(url, target) {
   $.getJSON(url, {}, function(data) {
-    $('li.icon-happy').text(data['ratings'].happy);
-    $('li.icon-meh').text(data['ratings'].meh);
-    $('li.icon-sad').text(data['ratings'].unhappy);
+    $('li.icon-emoji-happy').text(data['ratings'].happy);
+    $('li.icon-emoji-neutral').text(data['ratings'].meh);
+    $('li.icon-emoji-sad').text(data['ratings'].unhappy);
   });
 };
